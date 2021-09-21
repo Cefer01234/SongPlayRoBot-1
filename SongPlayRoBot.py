1aa871897694b65dca8a2129fd17b2e3109572ef
Cefer01234 <86205351+Cefer01234@users.noreply.github.com>
2021-09-21 13:19:39 +0400

    Update SongPlayRoBot.py

--- SongPlayRoBot.py
+++ SongPlayRoBot.py
@@ -30,15 +30,15 @@ def time_to_seconds(time):
 ## Commands --------------------------------
 @bot.on_message(filters.command(['start']))
 def start(client, message):
-    TamilBots = f'👋 𝗛𝗲𝗹𝗹𝗼 @{message.from_user.username}\n\n𝗜 𝗔𝗺 🎸𝐒𝐨𝐧𝐠 𝐏𝐥𝐚𝐲 𝐁𝐨𝐭[🎶](https://telegra.ph/file/6cb884fe1cb943ec12df1.mp4)\n\n𝗦𝗲𝗻𝗱 𝗧𝗵𝗲 𝗡𝗮𝗺𝗲 𝗢𝗳 𝗧𝗵𝗲 𝗦𝗼𝗻𝗴 𝗬𝗼𝘂 𝗪𝗮𝗻𝘁... 😍🥰🤗\n\n𝗧𝘆𝗽𝗲 /s 𝗦𝗼𝗻𝗴 𝗡𝗮𝗺𝗲\n\n𝐄𝐠. `/s Faded`'
+    TamilBots = f'👋 𝗦𝗮𝗹𝗮𝗺 @{message.from_user.username}\n\nMən AKM Song Botam[🎶](https://telegra.ph/file/fb66d3f27812c118fa19e.jpg)\n\İstədiyiniz Mahnının adıni mənə göndərin...🎶🎶\n\n𝗧𝘆𝗽𝗲 /s Mahnı adı\n\nMəsələn. `/song mahnı adı`'
     message.reply_text(
         text=TamilBots, 
         quote=False,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton('𝐒𝐔𝐏𝐏𝐎𝐑𝐓 👬', url='https://t.me/TamilSupport'),
-                    InlineKeyboardButton('𝐀𝐃𝐃 𝐌𝐄 🤗', url='https://t.me/SongProBot?startgroup=true')
+                    InlineKeyboardButton('𝐒𝐚𝐡𝐢𝐛', url='https://t.me/shirnovff'),
+                    InlineKeyboardButton('𝐌𝐞𝐧𝐢 𝐐𝐫𝐮𝐩𝐚 𝐞𝐥𝐚𝐯𝐞 𝐞𝐭 🤗', url='https://t.me/AkmSongBot?startgroup=true')
                 ]
             ]
         )
@@ -50,7 +50,7 @@ def a(client, message):
     for i in message.command[1:]:
         query += ' ' + str(i)
     print(query)
-    m = message.reply('🔎 𝐒𝐞𝐚𝐫𝐜𝐡𝐢𝐧𝐠 𝐭𝐡𝐞 𝐬𝐨𝐧𝐠...')
+    m = message.reply('🔎 Mahni Axtarılır...')
     ydl_opts = {"format": "bestaudio[ext=m4a]"}
     try:
         results = []
@@ -88,7 +88,7 @@ def a(client, message):
         )
         print(str(e))
         return
-    m.edit("🔎 𝐅𝐢𝐧𝐝𝐢𝐧𝐠 𝐀 𝐒𝐨𝐧𝐠 🎶 𝐏𝐥𝐞𝐚𝐬𝐞 𝐖𝐚𝐢𝐭 ⏳️ 𝐅𝐨𝐫 𝐅𝐞𝐰 𝐒𝐞𝐜𝐨𝐧𝐝𝐬 [🚀](https://telegra.ph/file/67f41ae52a85dfc0551ae.mp4)")
+    m.edit("🔎 𝐌𝐚𝐡𝐧𝐢 𝐓𝐚𝐩𝐢𝐥𝐢𝐫 🎶 𝐙𝐞𝐡𝐦𝐞𝐭 𝐎𝐥𝐦𝐚𝐬𝐚 ⏳️ 𝐁𝐢𝐫 𝐝𝐞𝐪 𝐠𝐨𝐳𝐥𝐞𝐲𝐢𝐧[🚀](https://telegra.ph/file/fb66d3f27812c118fa19e.jpg)")
     try:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
@@ -102,7 +102,7 @@ def a(client, message):
         message.reply_audio(audio_file, caption=rep, parse_mode='md',quote=False, title=title, duration=dur, thumb=thumb_name)
         m.delete()
     except Exception as e:
-        m.edit('❌ 𝐄𝐫𝐫𝐨𝐫\n\n Report This Erorr To Fix @TamilSupport ❤️')
+        m.edit('❌ 𝐗𝐞𝐭𝐚\n\n Report This Erorr To Fix @söhbət_akm ❤️')
         print(e)
     try:
         os.remove(audio_file)
